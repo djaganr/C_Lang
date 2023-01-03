cfb7fdbf1cc41a2fcaa8372841c0a7f6b057b412
djaganr <jaganr@apra.in>
2023-01-03 19:50:56 +0530

    func and return topic

--- functions/function.c
+++ functions/function.c
@@ -2,13 +2,20 @@
 #include <stdlib.h>
 
 
-void sayHi(char name[]){
-    printf("%s hi \n", name);
+void sayHi(char name[], int age){
+    printf("%s hi , you are %d \n", name, age);
 }
+
+double cube(double num){
+    return num*num*num;
+}
+
+
 
 int main(){
 
-    sayHi("Mike");
+    sayHi("Mike", 30);
+    printf("%f \n",cube(3));
     return 0;
 }
 
