c60032199396c2d1fce50ec76101fdfd55d16586
djaganr <jaganr@apra.in>
2023-01-04 10:44:27 +0530

    arrays completed'

---
+++ Arrays/arrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    char chapter[20] = "Arrays";
+    int numbers[] = {2, 5, 19};
+    printf("%d \n", numbers[1]);
+    printf("this is %s section \n", chapter);
+
+    
+
+    return 0;
+}
